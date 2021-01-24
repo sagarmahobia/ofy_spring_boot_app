5949cce3abb095b1fd9760b7d2f1faa3fe974343
sagarmahobia <sagarmahobia5@gmail.com>
2021-01-24 13:42:26 +0530

    added cross origin.

--- src/main/java/com/ryf/appbackend/core/controller/open/ArticleController.java
+++ src/main/java/com/ryf/appbackend/core/controller/open/ArticleController.java
@@ -2,14 +2,15 @@ package com.ryf.appbackend.core.controller.open;
 
 import com.ryf.appbackend.core.services.ArticleService;
 import com.ryf.appbackend.models.dto.Article;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class ArticleController {
 
     private final ArticleService articleService;
